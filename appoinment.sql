95683740bd956eca90b44601d8a7682ac3f2109d
silencer929 <petermbugua929@gmail.com>
2021-10-20 19:14:58 +0300

    presentation docs and sql

---
+++ appoinment.sql
@@ -0,0 +1,453 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 20, 2021 at 06:08 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appoinment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bloodgroup`
+--
+
+CREATE TABLE `bloodgroup` (
+  `bg_id` int(11) NOT NULL,
+  `bg_name` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bloodgroup`
+--
+
+INSERT INTO `bloodgroup` (`bg_id`, `bg_name`) VALUES
+(1, 'O+'),
+(2, 'O-'),
+(3, 'AB+'),
+(4, 'AB-'),
+(5, 'A+'),
+(6, 'A-'),
+(7, 'B+'),
+(8, 'B-');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `booking_id` int(22) NOT NULL,
+  `dname` varchar(22) NOT NULL,
+  `userid` int(22) NOT NULL,
+  `dcontact` varchar(22) NOT NULL,
+  `expertise` varchar(22) NOT NULL,
+  `fee` varchar(22) NOT NULL,
+  `pname` varchar(22) NOT NULL,
+  `pcontact` varchar(22) NOT NULL,
+  `email` varchar(111) NOT NULL,
+  `address` varchar(22) NOT NULL,
+  `dates` date NOT NULL,
+  `tyme` varchar(22) NOT NULL,
+  `payment` varchar(22) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`booking_id`, `dname`, `userid`, `dcontact`, `expertise`, `fee`, `pname`, `pcontact`, `email`, `address`, `dates`, `tyme`, `payment`) VALUES
+(24, 'Md.Gathoni', 1016, '01521470368', 'Bone', '800', 'peter', '0798614280', 'petermbugua929@gmail.com', 'agagag', '2021-10-29', '11.00am', 'Rocket');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(22) NOT NULL,
+  `cat` varchar(22) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `cat`) VALUES
+(1, 'Medicine'),
+(2, 'Heart'),
+(3, 'Bone'),
+(4, 'Kedney'),
+(5, 'Cardiologist'),
+(6, 'Plastic Surgeon'),
+(7, 'General Physician'),
+(8, 'Neurologist');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `contact_id` int(11) NOT NULL,
+  `firstname` varchar(10) NOT NULL,
+  `lastname` varchar(10) NOT NULL,
+  `email` varchar(15) NOT NULL,
+  `comment` varchar(111) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`contact_id`, `firstname`, `lastname`, `email`, `comment`) VALUES
+(1, 'Azharul', 'Islam', 'azad.ece13@gmai', 'My comment is getting successful.'),
+(2, 'zahid', 'hasan', 'hasan@gmail.com', 'good job');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `doc_id` int(22) NOT NULL,
+  `doctor_id` varchar(22) NOT NULL,
+  `name` varchar(22) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contact` varchar(14) NOT NULL,
+  `email` varchar(22) NOT NULL,
+  `expertise` varchar(22) NOT NULL,
+  `id` int(11) NOT NULL,
+  `fee` varchar(111) NOT NULL,
+  `userid` varchar(22) NOT NULL,
+  `password` varchar(22) NOT NULL,
+  `pic` varchar(111) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`doc_id`, `doctor_id`, `name`, `address`, `contact`, `email`, `expertise`, `id`, `fee`, `userid`, `password`, `pic`) VALUES
+(1, '2', 'Dr. Jane Onyango', 'nairobi', '01734761999', 'jane@gmail.com', 'Heart', 2, '500', '1001', '123', ''),
+(2, '1', 'Dr. Patrick Mwangi', 'Kagemi', '01764761919', 'patrick@gmail.com', 'Medicine', 1, '500', '1002', '123', 'doctor5.jpg'),
+(3, '1', 'Dr. Eliud Murage', 'Karen', '01521670654', 'eliudmurage@gmail.com', 'Medicine', 1, '600', '1003', '123', 'doctor1.jpg'),
+(4, '4', 'Dr.Muhamed Hussein', 'westlands', '01949389983', 'hussein@gmail.com', 'Kedney', 4, '700', '1004', '123', 'doctor5.jpg'),
+(5, '5', 'Dr. Nur Mohammad', 'Rongai', '01674546856', 'nurmohammad@gmail.com', 'Cardiologist', 5, '700', '1005', '123', 'itsme.jpg'),
+(6, '1', 'Dr. John Nyakundi', 'Umoja One', '01734761999', 'nyakundi@gmail.com', 'Medicine', 1, '800', '1006', '123', 'itzme.jpg'),
+(7, '8', 'Dr. Rafael Othiambo', 'Nairobib', '01674546563', 'rafael@gmail.com', 'Neurologist', 8, '600', '1007', '123', 'doctor5.jpg'),
+(8, '3', 'Dr. Linda Mukami', 'Mathare', '01521768936', 'lindamukami@gmail.com', 'Bone', 3, '700', '1008', '123', 'doctor1.jpg'),
+(9, '6', 'Dr. Abel Maina', 'Westlands', '+8801976564536', 'ablemaina@gmail.com', 'Plastic Surgeon', 6, '500', '1009', '123', 'doctor5.jpg'),
+(11, '11', 'Dr. Racheal ', 'Kangemi', '01764761919', 'racheal@gmail.com', 'Neurologist', 0, '800', '1012', '123', 'doctor1.jpg'),
+(12, '11', 'Dr. Miriam', 'Roysambu', '01764761919', 'miriam@gmail.com', 'Heart', 0, '800', '1011', '123', 'doctor5.jpg'),
+(14, '', 'Md.Gathoni', 'Kangemi', '01521470368', 'gathoni@gmail.com', 'Bone', 0, '800', '1016', '123', 'itsme.jpg'),
+(16, '', 'Dr. Nameless', 'Umoja one', '01521470368', 'nameless@gmail.com', 'General Physician', 0, '1005', '1013', '123', ''),
+(17, '', 'Md. Lilian One', 'Muthaiga', '01764761919', 'lilian@gmail.com', 'Neurologist', 0, '600', '1015', '123', 'header3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donation`
+--
+
+CREATE TABLE `donation` (
+  `donation_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `unit` varchar(2) NOT NULL,
+  `dates` date NOT NULL,
+  `email` varchar(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `email` varchar(22) NOT NULL,
+  `feedback` varchar(22) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `email`, `feedback`) VALUES
+(1, 'azad.tee@gmail.com', 'easy way'),
+(2, 'azad.ece13@gmail.com', 'nice getting you');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL,
+  `name` varchar(22) NOT NULL,
+  `age` varchar(22) NOT NULL,
+  `contact` varchar(22) NOT NULL,
+  `address` varchar(22) NOT NULL,
+  `bgroup` varchar(22) NOT NULL,
+  `email` varchar(111) NOT NULL,
+  `password` varchar(22) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`id`, `name`, `age`, `contact`, `address`, `bgroup`, `email`, `password`) VALUES
+(1, 'patient1', '21', '01746314882', 'Dinajpur', 'O+', 'patient1@gmail.com', '1234'),
+(2, 'bobo', '22', '01746314882', 'Saidpur,Rangpur', 'AB+', 'bobo@gmail.com', '123'),
+(3, 'patient2', '22', '01765674567', 'Dinajpur', 'A-', 'patient2@gmail.com', '123'),
+(5, 'developer', '21', '01764761919', 'Dhaka', 'O+', 'developer@gmail.com', 'developerazad'),
+(6, 'jambi', '22', '01521470368', 'Dhaka', 'AB+', 'jambi@gmail.com', '1234'),
+(7, 'peter', '21', '0798614280', 'agagag', 'O+', 'petermbugua929@gmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `donar_id` int(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `age` varchar(100) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `type` varchar(22) NOT NULL,
+  `city` varchar(221) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `bgroup` varchar(100) NOT NULL,
+  `dates` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` int(100) NOT NULL,
+  `pic` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requestes`
+--
+
+CREATE TABLE `requestes` (
+  `reg_id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `age` int(100) NOT NULL,
+  `mobile` varchar(100) NOT NULL,
+  `bgroup` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `reqdate` date NOT NULL,
+  `detail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `requestes`
+--
+
+INSERT INTO `requestes` (`reg_id`, `name`, `gender`, `age`, `mobile`, `bgroup`, `email`, `reqdate`, `detail`) VALUES
+(3, 'admin', 'male', 33, '01764761919', '', 'g@gmail.com', '2022-01-19', 'save life'),
+(4, 'admin', 'male', 33, '01764761919', '', 'g@gmail.com', '2022-01-19', 'save life'),
+(5, 'sanaaa', 'female', 21, '017777777', 'A-', 'g@gmail.com', '2017-12-01', 'save life'),
+(6, 'sanaaa', 'female', 21, '017777777', 'A-', 'g@gmail.com', '2017-12-01', 'save life'),
+(7, 'sanaaa', 'female', 21, '017777777', 'A-', 'g@gmail.com', '2017-12-01', 'save life'),
+(8, 'sanaaa', 'female', 21, '017777777', 'A-', 'g@gmail.com', '2017-12-01', 'save life'),
+(9, 'mamun', 'male', 22, '01777777', 'AB+', 'mamun@gmail.com', '0000-00-00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `type`) VALUES
+('admin', 'admin', 'admin'),
+('adnim', '13020944', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `type`) VALUES
+('adnim', '13020944', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bloodgroup`
+--
+ALTER TABLE `bloodgroup`
+  ADD PRIMARY KEY (`bg_id`);
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`contact_id`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`doc_id`);
+
+--
+-- Indexes for table `donation`
+--
+ALTER TABLE `donation`
+  ADD PRIMARY KEY (`donation_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`donar_id`);
+
+--
+-- Indexes for table `requestes`
+--
+ALTER TABLE `requestes`
+  ADD PRIMARY KEY (`reg_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bloodgroup`
+--
+ALTER TABLE `bloodgroup`
+  MODIFY `bg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `booking_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `doc_id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `donation`
+--
+ALTER TABLE `donation`
+  MODIFY `donation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `donar_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
+
+--
+-- AUTO_INCREMENT for table `requestes`
+--
+ALTER TABLE `requestes`
+  MODIFY `reg_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
